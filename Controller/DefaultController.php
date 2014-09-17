88d5e4aa4418c08ed8df7ac87015d939be0a25e9
soerenxrow <soeren@xrow.de>
2014-09-17 11:37:40 +0200

    - added override for SearchService

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -10,4 +10,26 @@ class DefaultController extends Controller
     {
         return $this->render('xrowEzPublishSolrDocsBundle:Default:index.html.twig', array('name' => $name));
     }
+	
+	    public function solrDocViewAction( $remoteid )
+    {
+        $repository = $this->container->get( 'ezpublish.solrapi.repository' );
+        $searchService = $repository->getSearchService();
+        $text = $remoteid;
+        $query = new \eZ\Publish\API\Repository\Values\Content\Query();
+        $query->criterion = new \eZ\Publish\API\Repository\Values\Content\Query\Criterion\FullText( $text );
+        $result = $searchService->findContent( $query, array(), false );
+        
+        $request = $this->container->get('request');
+        $response = new Response();
+        
+        
+        return $this->render("xrowEzPublishSolrDocsBundle:Default:solrdocviewsartikel.html.twig", array(
+                'pagelayout' => "eZDemoBundle::pagelayout.html.twig",
+                'route' => $request->get('_route'),
+                'context' => $remoteid,
+                'result' => $result,
+                'current_locale' => "ger-DE"
+        ), $response);
+    }
 }
